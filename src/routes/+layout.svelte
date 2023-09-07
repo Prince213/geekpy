2de5fc8dcc1f2ae6b92b5fc949f05e47fca8981c
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-07 12:25:15 +0800

    feat: geekpy python header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Header, Content, SkipToContent } from 'carbon-components-svelte';
+	import { onMount, setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const theme = writable('white');
+	setContext('theme', theme);
+
+	onMount(() => {
+		theme.subscribe((value) => document?.documentElement?.setAttribute('theme', value));
+	});
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://unpkg.com/carbon-components-svelte/css/all.css" />
+</svelte:head>
+<Header href="/">
+	<svelte:fragment slot="platform">{'{Geekpy: 🐍}'}</svelte:fragment>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+</Header>
+<Content>
+	<slot />
+</Content>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
+
+	const theme: Writable<string> = getContext('theme');
+
+	const changeTheme = () => {
+		theme.update((current) => (current === 'white' ? 'g100' : 'white'));
+	};
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={changeTheme}>x</button>
