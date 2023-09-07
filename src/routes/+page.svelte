1e62850e72549b084cc01a661e23104fa1ea5b56
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-07 12:31:21 +0800

    feat: jslinux with geekpy distro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
+	import { getContext, onMount } from 'svelte';
 	import type { Writable } from 'svelte/store';
 
 	const theme: Writable<string> = getContext('theme');
@@ -7,8 +7,28 @@
 	const changeTheme = () => {
 		theme.update((current) => (current === 'white' ? 'g100' : 'white'));
 	};
+
+	onMount(async () => {
+		await import('$lib/os/term.js');
+		await import('$lib/os/jslinux.js');
+	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={changeTheme}>x</button>
+
+<div id="term_wrap">
+	<div id="term_container" />
+	<div>
+		<textarea id="term_paste" cols="10" rows="1" autocorrect="off">Paste Here</textarea>
+		<label>
+			<img title="Upload files" src="public/images/upload-icon.png" /><input
+				type="file"
+				id="files"
+				multiple
+			/>
+		</label>
+		<progress id="net_progress" />
+	</div>
+</div>
