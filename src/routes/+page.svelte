ee096b4379169e5e1f03941e5f088fbe934b538f
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-09 00:40:05 +0800

    feat: geekpy executor skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,42 @@
 <script lang="ts">
-	import Terminal from '$lib/componets/Terminal.svelte';
 	import PyLinux from '$lib/pylinux';
-	import { getContext } from 'svelte';
+	import { getContext, onMount } from 'svelte';
+	import { env } from '$env/dynamic/public';
 
-	const changeTheme = getContext('changeTheme') as () => void;
+	const notifyError = getContext('notifyError') as (msg: string) => void;
+
+	let code = '';
+	let error: string | null = null;
+
+	let initialized = false;
+	let pylinux: PyLinux;
+
+	const run = async () => {
+		error = null;
+		let escaped = JSON.stringify(code);
+		await pylinux.exec(`echo -e ${escaped} > main.py`);
+		if (!(await pylinux.calibrate())) {
+			error = 'Geekpy engine loading script failed.';
+			return;
+		}
+		const result = await pylinux.exec('python main.py');
+		console.log(result);
+	};
+
+	onMount(async () => {
+		pylinux = await new PyLinux(env.PUBLIC_RISCVEMU_CONFIG).boot();
+		globalThis.pylinux = pylinux;
+		if (!pylinux.calibrate('python example.py', 'Hello world')) {
+			notifyError('Geekpy engine initialization failed.');
+			return;
+		}
+		initialized = true;
+	});
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button on:click={changeTheme}>x</button>
-<Terminal Base={PyLinux} />
+{#if initialized}
+	<textarea bind:value={code} />
+	<button on:click={run}>Run</button>
+{/if}
