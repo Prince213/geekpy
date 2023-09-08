634bc7583ef518b0714a0cc670926b98fa48a317
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-08 23:12:01 +0800

    feat: callibration in pylinux

--- src/lib/pylinux/index.ts
+++ src/lib/pylinux/index.ts
@@ -1,6 +1,7 @@
 import PyTerminal from '$lib/pylinux/terminal';
 import { Linux } from '$lib/jslinux';
 
+const MAGIC = /\r\n\[(geekpy)|(root)@localhost .+\]\$ $/;
 export default class PyLinux extends Linux(PyTerminal) {
 	constructor(config: string) {
 		super(config);
@@ -11,12 +12,23 @@ export default class PyLinux extends Linux(PyTerminal) {
 	}
 
 	public async exec(text: string): Promise<string> {
-		const regex = /\r\n\[(geekpy)|(root)@localhost .+\]\$ $/;
-		const promise = this.terminal.wait((str) => regex.test(str));
+		const promise = this.terminal.wait((str) => MAGIC.test(str));
 		this.cmd(text);
 		let result = await promise;
 		result = result.split('\r\n').slice(1, -1).join('\r\n');
 		return result;
+	}
+
+	public async calibrate(cmd = 'echo $?', expect = '0'): Promise<boolean> {
+		const result = await this.exec(cmd);
+		return result === expect;
+	}
+
+	public async boot(): Promise<this> {
+		const promise = this.terminal.wait((str) => MAGIC.test(str));
+		await super.boot();
+		await promise;
+		return this;
 	}
 }
 export { PyTerminal };
