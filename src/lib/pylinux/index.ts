6b5edfa066cf9b8876f3ed5c45b4ac65320dbf14
yanglinshu <nicolasvandarkholme@outlook.com>
2023-09-09 00:18:41 +0800

    fix: wrong filtering of output

--- src/lib/pylinux/index.ts
+++ src/lib/pylinux/index.ts
@@ -1,7 +1,7 @@
 import PyTerminal from '$lib/pylinux/terminal';
 import { Linux } from '$lib/jslinux';
 
-const MAGIC = /\r\n\[(geekpy)|(root)@localhost .+\]\$ $/;
+const MAGIC = /\[(?:geekpy|root)@localhost .+\]\$ $/;
 export default class PyLinux extends Linux(PyTerminal) {
 	constructor(config: string) {
 		super(config);
@@ -15,7 +15,16 @@ export default class PyLinux extends Linux(PyTerminal) {
 		const promise = this.terminal.wait((str) => MAGIC.test(str));
 		this.cmd(text);
 		let result = await promise;
-		result = result.split('\r\n').slice(1, -1).join('\r\n');
+		result = result.substring(result.indexOf(text) + text.length + 1);
+		result = result.replace(MAGIC, '');
+		if (result.startsWith('\r') || result.startsWith('\n')) {
+			result = result.substring(1);
+		} else if (result.startsWith('\r\n')) {
+			result = result.substring(2);
+		}
+		if (result.endsWith('\r\n')) {
+			result = result.substring(0, result.length - 2);
+		}
 		return result;
 	}
 
